803175ee16eb3ea1b9ce073a240d2a96c527b0e2
Denver Peterson <denver.peterson@loopup.com>
2018-05-09 21:47:32 -0700

    update main.go

--- main.go
+++ main.go
@@ -4,7 +4,11 @@ import (
 	"encoding/csv"
 	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
@@ -22,6 +26,34 @@ func main() {
 	}
 	links := parseLines(lines)
 	fmt.Println(links)
+
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range links[1:] {
+		go fetch(url, ch) //start the goroutine
+	}
+	for range links[1:] {
+		fmt.Println(<-ch)
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err)
+		return
+	}
+
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v ", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
 }
 
 func parseLines(lines [][]string) []link {
